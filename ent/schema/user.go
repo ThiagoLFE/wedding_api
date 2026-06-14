7d3a892d5dfe0fe3e97b603fff0f9700e9c4d5a8
ThiagoLFE <thiago.rib1905@gmail.com>
2026-06-14 19:15:41 -0300

    first commit

---
+++ ent/schema/user.go
@@ -0,0 +1,26 @@
+package schema
+
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/schema/field"
+)
+
+// User holds the schema definition for the User entity.
+type Product struct {
+	ent.Schema
+}
+
+// Fields of the Product.
+func (Product) Fields() []ent.Field {
+	return []ent.Field{
+		field.String("title"),
+		field.String("reserved_by").Nillable(),
+		field.Text("image").Optional(),
+		field.Float("valor"),
+	}
+}
+
+// Edges of the Product.
+func (Product) Edges() []ent.Edge {
+	return nil
+}
---
+++ main.go
@@ -0,0 +1,30 @@
+package wedding_api
+
+import (
+	"context"
+	"log"
+
+	"wedding_api/ent"
+
+	_ "github.com/lib/pq"
+)
+
+func main() {
+
+	client, err := ent.Open(
+		"postgres",
+		"host=localhost port=5432 user=postgres dbname=wedding password=postgres sslmode=disable",
+	)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer client.Close()
+
+	// Cria tabelas se não existirem
+	if err := client.Schema.Create(context.Background()); err != nil {
+		log.Fatal(err)
+	}
+
+}
